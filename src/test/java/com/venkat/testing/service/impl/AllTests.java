0b6025005157f275a823891ac83704b310278408
venkatram64 <venkat.veerareddy@hotmail.com>
2017-08-20 11:48:20 +0530

    init commit

---
+++ src/test/java/com/venkat/testing/service/impl/AllTests.java
@@ -0,0 +1,13 @@
+package com.venkat.testing.service.impl;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by Venkatram on 8/20/2017.
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({CalculatorTest.class, CalculatorParameterizedTest.class})
+public class AllTests {
+}
